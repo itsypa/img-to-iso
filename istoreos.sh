c15a2c996e240b63894f7a99160d15acd8b0bd53
itsypa <132572291+itsypa@users.noreply.github.com>
2026-01-14 22:59:21 +0800

    Update istoreos.sh

--- istoreos.sh
+++ istoreos.sh
@@ -12,7 +12,7 @@ mkdir -p openwrt
 
 # 从当前release获取带版本号的img.gz文件
 REPO="${GITHUB_REPOSITORY:-itsypa/img-installer}"
-TAG="$VERSION"
+TAG="istoreos"
 
 # 查找带版本号的img.gz文件，支持istoreos+版本号.img.gz格式
 FILE_NAME=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG | jq -r '.assets[] | select(.name | test("^istoreos.*\.img\.gz$")) | .name' | head -1)
